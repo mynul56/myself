b3073d978bb3c0fd74bc9bf35b19ec9e5bd9c914
mynul56 <mynulislamtanim@gmail.com>
2025-05-21 02:26:11 +0600

    okay

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/myself/',
+  base: mode === 'production' ? '/myself/' : '/',
   server: {
     host: "::",
     port: 8080,
